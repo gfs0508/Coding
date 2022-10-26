f6fe804197f1c902ead70cbd32ddcbe5b208609a
GFS-0508 <88109863+GFS-0508@users.noreply.github.com>
2022-10-26 16:29:46 +0100

    Delete HowToUse.py

--- Coding Strings/HowToUse.py
+++
@@ -1,9 +0,0 @@
-from coding import *
-
-var = "string to encoder"
-
-var_enc = code.encoder(var) # encoding string "var"
-print(var_enc)
-
-var_dec = code.decoder(var_enc) # decoding string "var"
-print(var_dec)
